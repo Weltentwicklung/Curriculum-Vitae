75fd0b9529cc6f59e3c6872c958943b885cbdba1
Weltentwicklung <stevil@gmx.ch>
2026-05-28 15:22:57 +0200

    added the whole introduction, education and practical experience part with minimal CSS styling

---
+++ docs/Scripts/js.js
@@ -0,0 +1,23 @@
+
+
+
+
+/*
+
+const response = await fetch(
+    "openplzapi.org/ch/Localities?postalCode={Postleitzahl}&name={Ortsname}" + encodeURIComponent(query) //(nimmt url untaugliche zeichen raus)!!!
+)
+
+
+if (!response.ok) -> wenn mit 2 beginnt
+
+
+xx = response.jason
+
+Ort= xx.ort
+plz= xx.plz */
+/*
+loop trough all 
+
+add it to a list display it in a drop down list
+*/ 
