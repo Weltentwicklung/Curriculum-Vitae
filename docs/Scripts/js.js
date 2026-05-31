04b6bcc9b2fe982dae9077702097491629092571
Weltentwicklung <stevil@gmx.ch>
2026-05-31 06:53:40 +0200

    added the general styling for the game window

--- docs/Scripts/js.js
+++ docs/Scripts/js.js
@@ -7,9 +7,49 @@
    -------------------------------------------------------------- */
 const imageGamePopup = document.getElementById("image-game-popup");
 
-document.getElementById("image-game-button").addEventListener("click", () => {imageGamePopup.showModal();});
+document.getElementById("image-game-button").addEventListener("click", () => {
+  imageGamePopup.showModal();
+  imageGamePopup.scrollTo(0, 0);
+  const canvas = document.getElementById("canvas-box");
+  const puzzle = document.getElementById("puzzle-box");
+
+  setGameBoxGrid(imageGamePopup);
+  calculateBoxSize(canvas);
+  calculateBoxSize(puzzle);
+
+  const observer = new ResizeObserver(() => {
+    setGameBoxGrid(imageGamePopup);
+    calculateBoxSize(canvas);
+    calculateBoxSize(puzzle);
+  });
+
+  observer.observe(imageGamePopup);
+});
 
 document.getElementById("close-image-game").addEventListener("click", () => {imageGamePopup.close();});
+
+   /* --------------------------------------------------------------
+        Styling
+   -------------------------------------------------------------- */
+function calculateBoxSize(element) {
+  if(element.parentElement.offsetWidth < element.parentElement.offsetHeight) {
+    element.style.width = "95%";
+  }
+  else {
+    element.style.height = "95%";
+  }
+}
+
+function setGameBoxGrid(element) {
+  if(element.offsetHeight > (1.2 * element.offsetWidth)) {
+    element.classList.add("dialog-field-stacked");
+    element.classList.remove("dialog-field-wide");
+  }
+  else {
+    element.classList.add("dialog-field-wide");
+    element.classList.remove("dialog-field-stacked");
+  }
+}
 
 
 
